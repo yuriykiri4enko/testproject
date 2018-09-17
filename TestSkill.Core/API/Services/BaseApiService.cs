289e819ed450b12d7a73f36291a0e33296939ed6
yuriykiri4enko <yuriykiri4enko@gmail.com>
2018-09-17 22:42:40 +0300

    remove useless request method

--- TestSkill.Core/API/Services/BaseApiService.cs
+++ TestSkill.Core/API/Services/BaseApiService.cs
@@ -27,30 +27,7 @@ namespace TestSkill.Core.API.Services
             }
         }
 
-        private async Task<HttpResponseMessage> ExecutePostAsync(string url, object keys)
-        {
-            HttpClient httpClient = CreateHttpClient();
-           
-            if (keys.GetType() == typeof(List<KeyValuePair<string, string>>))
-            {
-                var response = await httpClient.PostAsync(url, new FormUrlEncodedContent((List<KeyValuePair<string, string>>)keys));
-                return response;
-            }
-            else
-            {
-                httpClient.DefaultRequestHeaders.Accept.Clear();
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var settings = new JsonSerializerSettings
-                {
-                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                    NullValueHandling = NullValueHandling.Ignore
-                };
-
-                var body = JsonConvert.SerializeObject(keys, Newtonsoft.Json.Formatting.Indented, settings);
-                var response = await httpClient.PostAsync(url, new StringContent(body, Encoding.Unicode, "application/json"));
-                return response;
-            }
-        }
+      
 
         public async Task<HttpResponseMessage> ExecuteGetAsync(string url)
         {
