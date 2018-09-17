c13f2f8ae005b30d0ba1d6da385d2260e82a0498
yuriykiri4enko <yuriykiri4enko@gmail.com>
2018-09-17 16:55:03 +0300

    Revert "delete empty project"
    This reverts commit 217a2a22763a3c1c0c5bbf2a23105b42f8feebf3.

---
+++ TestSkill/TestSkill.Android/Setup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross;
+using MvvmCross.Forms.Platforms.Android.Core;
+using MvvmCross.Forms.Presenters;
+using TestSkill.Core;
+using TestSkill.UI;
+
+namespace TestSkill.Droid
+{
+    public class Setup : MvxFormsAndroidSetup<CoreApp, FormsApp>
+    {
+        protected override IMvxFormsPagePresenter CreateFormsPagePresenter(IMvxFormsViewPresenter viewPresenter)
+        {
+            var formsPresenter = base.CreateFormsPagePresenter(viewPresenter);
+            Mvx.RegisterSingleton(formsPresenter);
+            return formsPresenter;
+        }
+    }
+}
---
+++ TestSkill/TestSkill.iOS/AppDelegate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using MvvmCross.Forms.Platforms.Ios.Core;
+using TestSkill.Core;
+using TestSkill.UI;
+using UIKit;
+
+namespace TestSkill.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : MvxFormsApplicationDelegate<MvxFormsIosSetup<CoreApp, FormsApp>, CoreApp, FormsApp>
+    {
+
+    }
+}
